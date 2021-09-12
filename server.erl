e8d5b7baa2a156c3b786cde28e06ef200812a6e2
Benjamin McGriff <mcgriff.benjamin@gmail.com>
2021-09-11 19:26:45 -0700

    Refactor APIs to suit gen_server mechs

--- server.erl
+++ server.erl
@@ -7,28 +7,66 @@
 -export([init/1, terminate/2, code_change/3]).
 -export([handle_call/3, handle_cast/2, handle_info/2]).
 
+-record(state, {
+            accounts
+        }).
 
-
--export([start/0, stop/0]).
--export([deposit/2]).
-
-start() ->
-    io:format("---> Starting program...~n"),
-    Pid = spawn(main, init, []),
-    register(main, Pid).
-
-init() ->
-    Accounts = dict:new(),
-    loop(Accounts).
+%% API functions
+start_link() ->
+    io:format("---> Starting program.~n"),
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 stop() ->
-    main ! terminate.
+    gen_server:stop(?MODULE).
 
-deposit(AccID, Amt) ->
-    main ! {deposit, AccID, Amt}.
+deposit(AccountId, Amount) ->
+    gen_server:call(?MODULE, {deposit, AccountId, Amount}).
 
-loop(Accounts) ->
-    receive
+balance(AccountId) ->
+    gen_server:call(?MODULE, {balance, AccountId}).
+
+withdraw(AccountId, Amount) ->
+    gen_server:call(?MODULE, {withdraw, AccountId, Amount}).
+
+%% ====== Internals =====
+init([]) ->
+  Accounts = dict:new(),
+  State = #state{accounts = Accounts},
+  {ok, State}.
+
+handle_call({deposit, AccountId, Amount}, _From, #state{
+                                                     accounts = Accounts
+                                                 } = State ->
+                                                     CurrentBalance = get_current_balance(AccountId, Accounts),
+                                                     Accounts1 = dict:store(
+                                                                     AccountId,
+                                                                     CurentBalance + Amount,
+                                                                     Accounts
+                                                                 ),
+                                                     {reply, ok, State#state{accounts = Accounts1}};
+
+handle_call({balance, AccountId}, _From, #state{
+                                             accounts = Accounts
+                                         } = State) ->
+                                             CurrentBalance = get_current_balance(AccountId, Accounts),
+                                             {reply, CurrentBalance, State};
+
+handle_call({withdraw, AccountId, Amount}, _From, #state{
+                                                      accounts = Accounts
+                                                  } = State) ->
+                                                      case get_current_balance(AccountId, Accounts) of
+                                                          CurrentBalance when Amount =< CurrentBalance ->
+                                                              Accounts1 = dict:store(
+                                                                              AccountId,
+                                                                              CurrentBalance - Amount,
+                                                                              Accounts
+                                                                          ),
+                                                                          {reply, ok, State#state{accounts = Accounts1}};
+                                                          _-> {reply, {error, insufficient_funds}, State}
+                                                      end;
+
+
+
         {deposit, AccID, Amt} ->
             CurrentBalance = case dict:find(AccID, Accounts) of
                                  error -> 0;
