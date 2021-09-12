e3dfe78dd7efbb57f4bc3feb1d587294608451c1
Benjamin McGriff <mcgriff.benjamin@gmail.com>
2021-09-11 18:39:59 -0700

    Replace [init,terminate] functions with gen_server callbacks

--- server.erl
+++ server.erl
@@ -1,7 +1,15 @@
--module(main).
+%% @author Benjamin McGriff <mcgriff.benjamin@gmail.com>
+
+-module(server).
+-behavior(gen_server).
+
+%% ===== gen_server callbacks =====
+-export([init/1, terminate/2, code_change/3]).
+-export([handle_call/3, handle_cast/2, handle_info/2]).
+
+
 
 -export([start/0, stop/0]).
--export([init/0]).
 -export([deposit/2]).
 
 start() ->
