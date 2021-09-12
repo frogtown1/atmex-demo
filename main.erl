3e9ecbfa5445055833b08d16adb1d280952399c4
Benjamin McGriff <mcgriff.benjamin@gmail.com>
2021-09-11 18:32:48 -0700

    Abstract loop/0 under init/0; Add deposit/2

--- main.erl
+++ main.erl
@@ -1,9 +1,37 @@
 -module(main).
 
--export([start/0, stop/0, loop/0]).
+-export([start/0, stop/0]).
+-export([init/0]).
+-export([deposit/2]).
 
-start().
+start() ->
+    io:format("---> Starting program...~n"),
+    Pid = spawn(main, init, []),
+    register(main, Pid).
 
-stop().
+init() ->
+    Accounts = dict:new(),
+    loop(Accounts).
 
-loop().
+stop() ->
+    main ! terminate.
+
+deposit(AccID, Amt) ->
+    main ! {deposit, AccID, Amt}.
+
+loop(Accounts) ->
+    receive
+        {deposit, AccID, Amt} ->
+            CurrentBalance = case dict:find(AccID, Accounts) of
+                                 error -> 0;
+                                 {ok, Amt0} -> Amt0
+                             end,
+            Accounts1 = dict:store(
+                            AccID,
+                            CurrentBalance + Amt,
+                            Accounts
+                        ),
+            io:format("---> ~p deposited ~p.~n", [AccID, Amt]),
+            loop(Accounts1);
+        terminate -> io:format("---> Exiting program...~n")
+    end.
